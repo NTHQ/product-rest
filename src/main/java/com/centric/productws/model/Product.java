de256ae748dfaead673cc0db6ab3c590e7eac902
nthq <nguyen.thq.lewis@gmail.com>
2020-01-17 23:51:50 -0500

    Created Product model class

---
+++ src/main/java/com/centric/productws/model/Product.java
@@ -0,0 +1,70 @@
+package src.main.java.com.centric.productws.model;
+
+import java.util.Date;
+import java.util.List;
+
+public class Product {
+    private String id;
+    private String name;
+    private String description;
+    private String brand;
+    private List<String> tags;
+    private String category;
+    private Date created_at;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Date getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Date created_at) {
+        this.created_at = created_at;
+    }
+}
