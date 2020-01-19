f90c4b808422e191fc763c636f5b98dc42130bc9
nthq <nguyen.thq.lewis@gmail.com>
2020-01-18 22:02:47 -0500

    Fixed created_at format

--- src/main/java/com/centric/productws/controller/ProductController.java
+++ src/main/java/com/centric/productws/controller/ProductController.java
@@ -11,16 +11,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.sql.Timestamp;
 import java.util.List;
-import java.util.TimeZone;
 import java.util.UUID;
 
 @RestController
 public class ProductController {
-    final String ISO8601 = "yyyy-MM-dd'T'HH:mm'Z'";
     @Autowired
     private JdbcTemplate jdbc;
 
@@ -28,10 +24,8 @@ public class ProductController {
     public Product createProduct(String name, String description, String brand, String tags, String category) {
         final String QUERY = "INSERT INTO product (id, name, description, brand, tags, category, created_at) VALUES (?,?,?,?,?,?,?);";
         String productId = UUID.randomUUID().toString();
-        TimeZone tz = TimeZone.getTimeZone("UTC");
-        DateFormat df = new SimpleDateFormat(ISO8601);
-        df.setTimeZone(tz);
-        String createdAt = df.format(new Date());
+        Timestamp ts = new Timestamp(System.currentTimeMillis());
+        String createdAt = ts.toString();
 
         try {
             int num = jdbc.update(QUERY, new Object[]{productId, name, description, brand, tags, category, createdAt});
